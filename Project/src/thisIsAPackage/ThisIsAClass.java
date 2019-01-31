1ee86e4b0f9c7bd3cd01be99233f2bf30bca0938
oufttdl <oufttdl@CCSD-45FXBH2.ccsdut.net>
2019-01-31 10:05:48 -0700

    init commit

--- Project/src/thisIsAPackage/ThisIsAClass.java
+++ Project/src/thisIsAPackage/ThisIsAClass.java
@@ -5,6 +5,7 @@ public class ThisIsAClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("YOUR MAMMA!!!!!!!!!!!!!!");
+		System.out.println("wow");
 
 	}
 
