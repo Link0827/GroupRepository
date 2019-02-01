9efa3b985a38cefb699e847ae5de82d6d3b9d025
oufttdl <oufttdl@CCSD-45FXBH2.ccsdut.net>
2019-02-01 09:23:57 -0700

    ok fine then delete

--- Project/src/thisIsAPackage/ThisIsAClass.java
+++ Project/src/thisIsAPackage/ThisIsAClass.java
@@ -2,7 +2,8 @@ package thisIsAPackage;
 
 public class ThisIsAClass {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+{
 		// TODO Auto-generated method stub
 		System.out.println("YOUR MAMMA!!!!!!!!!!!!!!");
 		System.out.println("wow");
