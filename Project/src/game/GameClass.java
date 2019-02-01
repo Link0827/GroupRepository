23b7e4e56f62bdaa527bfc81ea023222efcc6ccb
oufttdl <oufttdl@CCSD-45FXBH2.ccsdut.net>
2019-02-01 09:39:57 -0700

    c

---
+++ Project/src/game/GameClass.java
@@ -0,0 +1,14 @@
+package game;
+
+public class GameClass 
+{
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		//This will be the class where all other classes come together to make the game
+
+	}
+
+}
