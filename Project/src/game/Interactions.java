2546c7bdee4e5bd9376ff30d0d139153e1057e78
OUFTTDL <OUFTTDL@CCSD-45FXBH2.ccsdut.net>
2019-02-04 10:14:17 -0700

    v

--- Project/src/game/Interactions.java
+++ Project/src/game/Interactions.java
@@ -2,11 +2,34 @@ package game;
 
 public class Interactions 
 {
-
-	public static void main(String[] args) 
+	private int inter;
+	
+	private static final String one = "Monsters";
+	private static final String two = "Trader";
+	private static final String three = "Nothing";
+	
+	
+	public Interactions(int num)
 	{
-		
-
+		inter = num;
 	}
+	
+	public String getInteraction()
+	{
+		if(inter == 0)
+		{
+			return one;
+		}
+		else if(inter == 1)
+		{
+			return two;
+		}
+		return three;
+		
+	}
+	
+	
+	
+
 
 }
