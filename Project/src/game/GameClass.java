beeeda849427d4b77ed0a6a762de69f23dc80e08
AWVWWTU <AWVWWTU@CCSD-45FYBH2.ccsdut.net>
2019-02-06 09:42:17 -0700

    I GOT IT!!!!!!!!!!!!!!!

--- Project/src/game/GameClass.java
+++ Project/src/game/GameClass.java
@@ -5,9 +5,7 @@ public class GameClass
 
 	public static void main(String[] args) 
 	{
-		// TODO Auto-generated method stub
 		
-		//This will be the class where all other classes come together to make the game
 
 	}
 
--- Project/src/thisIsAPackage/ThisIsAClass.java
+++ Project/src/thisIsAPackage/ThisIsAClass.java
@@ -1,18 +1,26 @@
 package thisIsAPackage;
 
-public class ThisIsAClass {
+import java.awt.Dimension;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+public class ThisIsAClass
+{
+	private static JFrame frame;
 
 	public static void main(String[] args) 
-{
-		// TODO Auto-generated method stub
-		System.out.println("YOUR MAMMA!!!!!!!!!!!!!!");
-		System.out.println("wow");
-		for(int i = 0; i < 5; i++)
-		{
-			System.out.println(i + " this is a number");
-		}
-
-
+	{
+	    JFrame jframe = new JFrame("JFrame Size Example");
+	    jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	    jframe.setPreferredSize(new Dimension(400, 300));
+	    jframe.pack();
+	    jframe.setLocationRelativeTo(null);
+	    jframe.setVisible(true);
+	    JButton button = new JButton("this be a button");
+	    jframe.add(button);
+		
 	}
-
+		
 }
+
