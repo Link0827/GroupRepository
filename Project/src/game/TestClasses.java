7aabadfbd0292443ca0f2387115a54cec4a02b2c
OUFTTDL <OUFTTDL@CCSD-45FXBH2.ccsdut.net>
2019-02-06 09:19:16 -0700

    s

--- Project/src/game/TestClasses.java
+++ Project/src/game/TestClasses.java
@@ -3,25 +3,33 @@ package game;
 import java.util.Random;
 import java.util.Scanner;
 
-public class TestInteractions {
+public class TestClasses {
 
 	public static void main(String[] args) 
 	{
 		// TODO Auto-generated method stub
+		HealthBar c = new HealthBar();
+		System.out.println(c.getHealth());
+		c.setHealth(30);
+		System.out.println(c.getHealth());
+		c.addHealth(-5);
+		System.out.println(c.getHealth());
+		System.out.println(c);
+		
 		HealthBar health = new HealthBar();
 		Interactions a = new Interactions(new Random().nextInt(3), health.getHealth());
 		
 		running(health, a);
 		System.out.println("do you want to keep traveling? answer t or f ");
 		Scanner scan = new Scanner(System.in);
-		String c = scan.nextLine();
-		while(c.equalsIgnoreCase("T"))
+		String c1 = scan.nextLine();
+		while(c1.equalsIgnoreCase("T"))
 		{
 			Interactions a1 = new Interactions(new Random().nextInt(3), health.getHealth());
 			running(health, a1);
 			System.out.println("do you want to keep traveling? answer t or f ");
 			Scanner scan1 = new Scanner(System.in);
-			String c1 = scan1.nextLine();
+			String c11 = scan1.nextLine();
 		}
 		
 
