59ae1f13b9aebeaf1151d8471a3975df4ed5cccb
OUFTTDL <OUFTTDL@CCSD-45FXBH2.ccsdut.net>
2019-02-19 10:17:04 -0700

    ag hvbqadefsvhcaewsdyqajvbdfknsatdanabvq

--- Project/src/game/GameClass.java
+++ Project/src/game/GameClass.java
@@ -1,12 +1,67 @@
 package game;
 
+import java.awt.Color;
+import java.awt.Frame;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
 public class GameClass 
 {
+	
+	String name;
 
 	public static void main(String[] args) 
 	{
-		
+		getName();
+		chooseGameStyle();
 
 	}
+	
+	
+	
+	
+	private static void chooseGameStyle() 
+	{
+		
+	}
+
+
+
+
+	public static void getName()
+	{
+		
+		JFrame frame = new JFrame("Name");
+		frame.setDefaultLookAndFeelDecorated(true);
+		JButton button = new JButton();
+		frame.
+		
+		JPanel panel = new JPanel();
+		
+		panel.setLayout();
+		
+		panel.setBackground(Color.GREEN);
+		
+		button.setText("When your name has been inputed please click");
+		
+		panel.add(button);
+		button.setLocation(500, 300);
+		
+		
+		frame.add(panel);
+		
+		frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+				
+		
+		
+	}
+	
+
 
 }
