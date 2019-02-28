b7612c63297406dbf437e295cd9ff9c4cf2ffcca
OUFTTDL <OUFTTDL@CCSD-45FXBH2.ccsdut.net>
2019-02-28 10:09:55 -0700

    AAAAAHHHHHHH I GOT IT

--- Project/src/game/GameClass.java
+++ Project/src/game/GameClass.java
@@ -2,12 +2,15 @@ package game;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+
 import java.awt.Dimension;
 import java.awt.Frame;
 import java.awt.Toolkit;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -17,78 +20,75 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
+import com.sun.xml.internal.bind.v2.TODO;
 
 
-public class GameClass 
+
+public class GameClass implements  ActionListener
 {
 	
-	
-	private static String name;
-	static JFrame frame;
-	static JTextField field;
+	static String name;
+	JTextField field1;
+	JFrame frame1;
+	JButton button1;
+	JPanel panel1;
+	JLabel label1;
+
 	public static void main(String[] args) 
 	{
-		getName();
-		testName();
+		GameClass game = new GameClass();
+		game.startGame();
+	}
+	public void startGame()
+	{
+		label1 = new JLabel("Put name here");
+		label1.setLocation(10, 0);
+		label1.setSize(100, 30);
+		
+		field1 = new JTextField();
+		field1.setSize(100, 30);
+		field1.setLocation(10, 30);
+		
+		button1 = new JButton("Set name");
+		button1.setSize(100, 30);
+		button1.setLocation(10, 60);
+		button1.addActionListener(this);
+		
+		
+		
+		panel1 = new JPanel();
+		panel1.setLayout(null);
+		panel1.add(label1);
+		panel1.add(button1);
+		panel1.add(field1);
+		panel1.setSize(500, 500);
+		
+		frame1 = new JFrame("Name");
+		frame1.setDefaultLookAndFeelDecorated(true);
+		frame1.add(panel1);
+		frame1.setSize(100, 140);
+		frame1.setLocationRelativeTo(null);
+		frame1.setResizable(true);
+        frame1.setVisible(true);
+        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame1.setVisible(true);
 
 	}
 	
+	@Override
+	public void actionPerformed(ActionEvent e) 
+	{
+		if(e.getSource() == button1)
+		{
+			name = field1.getText();
+        	frame1.dispose();
+        	System.out.println(name);
+        	
+		}
+		
+	}
+
 	
-	private int button;
-	public GameClass(int button)
-	{
-	    this.button = button;
-	}
-	public static void testName()
-	{
-		
-	}
-
-
-
-
-	public static void getName()
-	{
-		
-		frame = new JFrame("Name");
-		frame.setDefaultLookAndFeelDecorated(true);
-		JButton button = new JButton();
-		
-		JPanel panel = new JPanel();
-		JLabel label = new JLabel("Put name here");
-		label.setLocation(10, 0);
-		label.setSize(100, 30);
-		field = new JTextField();
-		field.setSize(100, 30);
-		field.setLocation(10, 30);
-		button.setText("Set name");
-		button.setSize(100, 30);
-		button.setLocation(10, 60);
-		panel.setLayout(null);
-		panel.add(label);
-		panel.add(button);
-		panel.add(field);
-		panel.setSize(500, 500);
-		button.addActionListener((ActionListener) new GameClass(1));
-		frame.add(panel);
-		frame.setSize(100, 140);
-		frame.setLocationRelativeTo(null);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setVisible(true);
-				
-        button.addActionListener(new ActionListener()
-        {
-            public void actionPerformed(ActionEvent e)
-            {
-                if(e.getSource().equals(1))
-                {
-                	name = field.getText();
-                }
-            }
-        });
-		
-		
-	}
 	
 
 
